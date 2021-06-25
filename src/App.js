6b29e5af2ccf708cb858b000d9a2b9c77c7acb18
sanket <sanketr43@gmail.com>
2021-06-25 10:41:29 +0530

    update our menu-imtes to navigate  withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.components';
+import { Route } from 'react-router-dom';
+
+const HatPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return <div>
-    <HomePage />
+    <Route exact path="/" component={HomePage} />
+    <Route exact path="/shop/hats" component={HatPage} />
   </div>;
 }
 

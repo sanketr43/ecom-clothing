1c86935f4ab0d7d7823dc3f6cdc0fd86b8f736cb
sanket <sanketr43@gmail.com>
2021-07-30 08:29:46 +0530

    added createSelector and convert cart and current user into createSelector

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,6 +6,10 @@ import { auth } from '../../firebase/firebase.utils';
 import { connect } from 'react-redux';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+import { selectCartHidden } from '../../redux/cart/cart.selectors';
+import { selectCurrentUser } from '../../redux/user/user.selectors';
+import { createStructuredSelector } from 'reselect';
+
 
 const Header = ({ currentUser, hidden }) => (
     <div className='header'>
@@ -36,9 +40,9 @@ const Header = ({ currentUser, hidden }) => (
 );
 
 
-const mapStateToProps = ({user: { currentUser },  cart: { hidden }}) => ({
-    currentUser,
-    hidden
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser,
+    hidden: selectCartHidden
 });
 
 export default connect(mapStateToProps)(Header);
